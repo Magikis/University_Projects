24ccd5cb7a63a6fbdf00e89558213c86827e4437
Magikis <kulczakkacper@gmail.com>
2018-02-19 19:20:25 +0100

    python pritier

--- Konferencja_DataBaseAPI/functions.py
+++ Konferencja_DataBaseAPI/functions.py
@@ -4,26 +4,29 @@ import psycopg2
 ok = 0
 err = 1
 
-def status( x ):
+
+def status(x):
     if x == ok:
         ansewer = "OK"
-    elif type(x) == type( [] ):
-        return {"status" : "OK", "data" : x}
-    else :
+    elif type(x) == type([]):
+        return {"status": "OK", "data": x}
+    else:
         ansewer = "ERROR"
-    return {"status" : ansewer}
+    return {"status": ansewer}
+
 
 def is_user(conn, dict):
     sql = "select * from users where login = %(login)s and password = %(password)s "
     row = read_one_rec(conn, sql, dict)
     return row != None
 
+
 def is_organizer(conn, dict):
     sql = "select * from users where login = %(login)s and password = %(password)s "
-    row = read_one_rec(conn,sql, dict)
+    row = read_one_rec(conn, sql, dict)
     if row == None:
         return False
-    return row[2]  == "organizer"
+    return row[2] == "organizer"
 
 
 def insert_record(conn, dict, sql):
@@ -39,26 +42,28 @@ def insert_record(conn, dict, sql):
         return status(err)
     return status(ok)
 
+
 def execute_querry(conn, dict, sql, ret_args):
     cur = conn.cursor()
     try:
         cur.execute(sql, dict)
-    except psycopg2.DatabaseError as e :
+    except psycopg2.DatabaseError as e:
         # print("Eception: " + str(e))
         cur.close()
         conn.rollback()
         return None
     query = cur.fetchall()
-    ansewer =   []
+    ansewer = []
     for row in query:
         d = {}
         for i in range(len(ret_args)):
-            d.update({ ret_args[i] : str(row[i]) })
+            d.update({ret_args[i]: str(row[i])})
         ansewer.append(d)
     return ansewer
 
+
 def read_one_rec(conn, sql, dict):
-    cur  = conn.cursor()
+    cur = conn.cursor()
     try:
         cur.execute(sql, dict)
     except psycopg2.DatabaseError as e:
@@ -68,41 +73,44 @@ def read_one_rec(conn, sql, dict):
     cur.close()
     return row
 
+
 def magic(sql):
     sql = sql.split(";")
-    return list( map(lambda x : x + ";" , sql) )
+    return list(map(lambda x: x + ";", sql))
+
 
 def open_db(dict):
     try:
-        conn =  psycopg2.connect("dbname={} user={} password={}".format(
-            dict.get("baza"), dict.get("login"), dict.get("password")  )  )
-    except psycopg2.DatabaseError as e :
+        conn = psycopg2.connect("dbname={} user={} password={}".format(
+            dict.get("baza"), dict.get("login"), dict.get("password")))
+    except psycopg2.DatabaseError as e:
         return None, status(err)
     sql = """select exists ( select * from INFORMATION_SCHEMA.TABLES
     where TABLE_NAME = 'users' ) """
     row = read_one_rec(conn, sql, dict)
-    if row != None and row[0] == False :
-        with open("model_fizyczny.sql", 'r') as file :
-            sql =  file.read().split("--")
+    if row != None and row[0] == False:
+        with open("model_fizyczny.sql", 'r') as file:
+            sql = file.read().split("--")
             sql.pop()
             file.close()
             for s in sql:
                 stat = insert_record(conn, dict, s)
                 if stat == status(err):
                     # print("Error on: " + s)
-                    return conn,stat
+                    return conn, stat
                 conn.commit()
 
     return conn, status(ok)
 # open <baza> <login> <password>
 
 
-def organizer( conn, dict ):
+def organizer(conn, dict):
     sql = "insert into users values ( %(newlogin)s , %(newpassword)s , 'organizer' )"
     if dict.get("secret") == "d8578edf8458ce06fbc5bb76a58c5ca4":
         return insert_record(conn, dict, sql)
     else:
         return status(err)
+
 
 def event(conn, dict):
     if not is_organizer(conn, dict):
@@ -110,22 +118,25 @@ def event(conn, dict):
     sql = "insert into event values ( %(eventname)s, %(start_timestamp)s, %(end_timestamp)s);"
     return insert_record(conn, dict, sql)
 
-def user(conn,dict):
+
+def user(conn, dict):
     if not is_organizer(conn, dict):
         return status(err)
     sql = " insert into users values ( %(newlogin)s, %(newpassword)s );"
     return insert_record(conn, dict, sql)
 
 # DO POPRAWIENIA
+
+
 def talk(conn, dict):
     if not is_organizer(conn, dict):
         return status(err)
     sql = "select talk,status from talk where talk = %(talk)s ;"
     row = read_one_rec(conn, sql, dict)
-    if  row != None and row[1] == 'rejected':
+    if row != None and row[1] == 'rejected':
         return status(err)
     if dict.get("eventname") == "":
-        dict.update({"eventname" : None})
+        dict.update({"eventname": None})
     sql = """ insert into talk (talk, start_time, room, title, status)
             VALUES ( %(talk)s, %(start_timestamp)s, %(room)s , %(title)s , 'accepted'  )
             ON CONFLICT (talk) DO update
@@ -147,13 +158,16 @@ def talk(conn, dict):
             values (%(login)s, %(talk)s, %(initial_evaluation)s )
             on CONFLICT do nothing; """
     return insert_record(conn, dict, sql)
-#KONIEC
+# KONIEC
 # talk <login> <password> <speakerlogin> <talk> <title> <start_timestamp> <room> <initial_evaluation> <eventname>
+
+
 def register_user_for_event(conn, dict):
     if not is_user(conn, dict):
         return status(err)
     sql = "insert into register values (%(login)s, %(eventname)s);"
     return insert_record(conn, dict, sql)
+
 
 def attendance(conn, dict):
     if not is_user(conn, dict):
@@ -162,6 +176,8 @@ def attendance(conn, dict):
     return insert_record(conn, dict, sql)
 
 # (*U) evaluation <login> <password> <talk> <rating> // ocena referatu <talk> w skali 0-10 przez uczestnika <login>
+
+
 def evaluation(conn, dict):
     if not is_user(conn, dict):
         return status(err)
@@ -175,8 +191,9 @@ def reject(conn, dict):
     sql = " update talk set status = 'rejected' where talk = %(talk)s ;"
     return insert_record(conn, dict, sql)
 
+
 def proposal(conn, dict):
-    if not is_user(conn,dict):
+    if not is_user(conn, dict):
         return status(err)
     sql = """ insert into talk values ( %(talk)s , %(start_timestamp)s,
      NULL , %(title)s, 'proposed' );
@@ -184,12 +201,15 @@ def proposal(conn, dict):
     return insert_record(conn, dict, sql)
 
 # want_to_be_friend trigger
+
+
 def friends(conn, dict):
-    dict.update({"login" : dict.get("login1")})
+    dict.update({"login": dict.get("login1")})
     if not is_user(conn, dict):
         return status(err)
     sql = " insert into want_to_be_friend values (%(login)s, %(login2)s );"
     return insert_record(conn, dict, sql)
+
 
 def user_plan(conn, dict):
     sql = """select pr.login, p.talk, p.start_time, p.title, p.room
@@ -203,7 +223,7 @@ def user_plan(conn, dict):
     if dict.get("start_timestamp") != 0:
         sql += " limit %(limit)s"
     ret_args = ["login", "talk", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn, dict, sql,ret_args))
+    return status(execute_querry(conn, dict, sql, ret_args))
 
 
 def day_plan(conn, dict):
@@ -212,8 +232,9 @@ def day_plan(conn, dict):
         where status = 'accepted'
         and %(timestamp)s::date = start_time::date
         order by room, start_time;"""
-    ret_args = [ "talk", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn,dict,sql,ret_args))
+    ret_args = ["talk", "start_timestamp", "title", "room"]
+    return status(execute_querry(conn, dict, sql, ret_args))
+
 
 def best_talks(conn, dict):
     sql = """select t.talk, t.start_time, t.title,
@@ -229,14 +250,15 @@ def best_talks(conn, dict):
             where type = 'organizer') ) ag using(talk)    """
     else:
         sql += """join evaluate e using(talk) """
-    sql +=  """where status = 'accepted' and start_time between
+    sql += """where status = 'accepted' and start_time between
             %(start_timestamp)s and %(end_timestamp)s
             group by t.talk, t.start_time, t.title, t.room
             order by avg(grade) desc """
     if dict.get("limit") != 0:
         sql += "limit %(limit)s "
     ret_args = ["talk", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn, dict, sql, ret_args) )
+    return status(execute_querry(conn, dict, sql, ret_args))
+
 
 def most_popular_talks(conn, dict):
     sql = """select t.talk, t.start_time, t.title, t.room from talk t join attend a using(talk)
@@ -247,18 +269,20 @@ def most_popular_talks(conn, dict):
     if dict.get("limit") != 0:
         sql += "limit %(limit)s "
     ret_args = ["talk", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn, dict, sql, ret_args) )
+    return status(execute_querry(conn, dict, sql, ret_args))
+
 
 def attended_talks(conn, dict):
-    if not is_user(conn , dict):
+    if not is_user(conn, dict):
         return status(err)
     sql = """select t.talk, t.start_time, t.title, t.room from talk t join attend a using(talk)
         where status = 'accepted' and login = %(login)s """
     ret_args = ["talk", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn, dict, sql, ret_args) )
+    return status(execute_querry(conn, dict, sql, ret_args))
 
-def abandoned_talks( conn, dict ):
-    if not is_organizer(conn , dict):
+
+def abandoned_talks(conn, dict):
+    if not is_organizer(conn, dict):
         return status(err)
     sql = """ select q.talk, t.start_time, t.title, t.room, count(q.login)
     from
@@ -274,10 +298,11 @@ def abandoned_talks( conn, dict ):
     order by count(q.login) desc"""
     if dict.get("limit") != 0:
         sql += " limit %(limit)s"
-    ret_args = [ "talk", "start_timestamp", "title", "room", "number" ]
+    ret_args = ["talk", "start_timestamp", "title", "room", "number"]
     return status(execute_querry(conn, dict, sql, ret_args))
 
-def recently_added_talks( conn, dict ):
+
+def recently_added_talks(conn, dict):
     sql = """ select t.talk, p.login, t.start_time, t.title, t.room
     from talk t join propose p using(talk)
     where t.status = 'accepted'
@@ -286,11 +311,12 @@ def recently_added_talks( conn, dict ):
     if dict.get("limit") != 0:
         sql += " limit %(limit)s"
     ret_args = ["talk", "speakerlogin", "start_timestamp", "title", "room"]
-    return status(execute_querry(conn, dict, sql, ret_args)  )
+    return status(execute_querry(conn, dict, sql, ret_args))
 
 # (U/O) rejected_talks <login> <password> // jeśli wywołujący ma uprawnienia organizatora zwraca listę wszystkich odrzuconych referatów spontanicznych,
 # w przeciwnym przypadku listę odrzuconych referatów wywołującego ją uczestnika
 # //  <talk> <speakerlogin> <start_timestamp> <title>
+
 
 def rejected_talks(conn, dict):
     add_sql = ""
@@ -302,21 +328,22 @@ def rejected_talks(conn, dict):
     sql = """select talk, login, start_time, title
             from talk join propose using(talk)
             where status = 'rejected' """ + add_sql
-    ret_args = [ "talk", "speakerlogin", "start_timestamp", "title" ]
-    return status( execute_querry(conn, dict, sql, ret_args) )
+    ret_args = ["talk", "speakerlogin", "start_timestamp", "title"]
+    return status(execute_querry(conn, dict, sql, ret_args))
 
     # (O) proposals <login> <password> // zwraca listę propozycji referatów spontanicznych do zatwierdzenia lub odrzucenia,
     # zatwierdzenie lub odrzucenie referatu polega na wywołaniu przez organizatora funkcji talk lub reject z odpowiednimi parametrami
     # //  <talk> <speakerlogin> <start_timestamp> <title>
-def proposals(conn ,dict):
+
+
+def proposals(conn, dict):
     if not is_organizer(conn, dict):
         return status(err)
     sql = """select talk, login, start_time, title
             from talk join propose using(talk)
             where status = 'proposed' """
-    ret_args = [ "talk", "speakerlogin", "start_timestamp", "title" ]
-    return status( execute_querry(conn, dict, sql, ret_args) )
-
+    ret_args = ["talk", "speakerlogin", "start_timestamp", "title"]
+    return status(execute_querry(conn, dict, sql, ret_args))
 
 
 # (U) friends_talks <login> <password> <start_timestamp> <end_timestamp> <limit> // lista referatów
@@ -336,15 +363,16 @@ def friends_talks(conn, dict):
            and %(end_timestamp)s"""
     if dict.get("limit") != 0:
         sql += " limit %(limit)s"
-    ret_args = [ "talk", "speakerlogin", "start_timestamp", "title", "room" ]
-    return status( execute_querry(conn, dict, sql, ret_args) )
+    ret_args = ["talk", "speakerlogin", "start_timestamp", "title", "room"]
+    return status(execute_querry(conn, dict, sql, ret_args))
 
 # (U) friends_events <login> <password> <eventname>
 # // lista znajomych uczestniczących w danym wydarzeniu
 # //  <login> <eventname> <friendlogin>
 
+
 def friends_events(conn, dict):
-    if not is_user(conn, dict) :
+    if not is_user(conn, dict):
         return status(err)
     sql = """select distinct %(login)s , event_name,f.has_friend
             from attend a join talk_in_event te using (talk)
@@ -352,8 +380,8 @@ def friends_events(conn, dict):
             where
               f.login = %(login)s
               and event_name = %(eventname)s """
-    ret_args = [ "login", "eventname", "friendlogin" ]
-    return status( execute_querry(conn, dict, sql, ret_args) )
+    ret_args = ["login", "eventname", "friendlogin"]
+    return status(execute_querry(conn, dict, sql, ret_args))
 
 
 # (U) recommended_talks <login> <password> <start_timestamp> <end_timestamp> <limit>
@@ -374,5 +402,6 @@ def recommended_talks(conn, dict):
         order by score(t.talk , %(login)s ) desc  """
     if dict.get("limit") != 0:
         sql += " limit %(limit)s"
-    ret_args = [ "talk", "speakerlogin", "start_timestamp", "title", "room", "score" ]
-    return status( execute_querry(conn, dict, sql, ret_args) )
+    ret_args = ["talk", "speakerlogin",
+                "start_timestamp", "title", "room", "score"]
+    return status(execute_querry(conn, dict, sql, ret_args))
--- Konferencja_DataBaseAPI/main.py
+++ Konferencja_DataBaseAPI/main.py
@@ -5,19 +5,36 @@ import functions as f
 
 
 aviable_fucntions = {
- "open": f.open_db , "organizer": f.organizer, "user" : f.user, "event" : f.event
- , "talk" : f.talk, "register_user_for_event" : f.register_user_for_event, "attendance" : f.attendance
- , "evaluation" : f.evaluation
- ,"reject" : f.reject, "proposal" : f.proposal, "friends" : f.friends, "user_plan" : f.user_plan
- ,"day_plan" : f.day_plan, "best_talks" : f.best_talks, "most_popular_talks" : f.most_popular_talks
- ,"attended_talks" : f.attended_talks, "abandoned_talks": f.abandoned_talks, "recently_added_talks" : f.recently_added_talks
- , "rejected_talks" : f.rejected_talks, "proposals": f.proposals, "friends_talks": f.friends_talks
- ,"friends_events": f.friends_events , "recommended_talks": f.recommended_talks
-  }
+    "open": f.open_db,
+    "organizer": f.organizer,
+    "user": f.user,
+    "event": f.event,
+    "talk": f.talk,
+    "register_user_for_event": f.register_user_for_event,
+    "attendance": f.attendance,
+    "evaluation": f.evaluation,
+    "reject": f.reject,
+    "proposal": f.proposal,
+    "friends": f.friends,
+    "user_plan": f.user_plan,
+    "day_plan": f.day_plan,
+    "best_talks": f.best_talks,
+    "most_popular_talks": f.most_popular_talks,
+    "attended_talks": f.attended_talks,
+    "abandoned_talks": f.abandoned_talks,
+    "recently_added_talks": f.recently_added_talks,
+    "rejected_talks": f.rejected_talks,
+    "proposals": f.proposals,
+    "friends_talks": f.friends_talks,
+    "friends_events": f.friends_events,
+    "recommended_talks": f.recommended_talks
+}
+
 
 def test(conn):
-    print(f.is_user(conn,{"login": 'a', "password" : 'c'}) )
+    print(f.is_user(conn, {"login": 'a', "password": 'c'}))
     return
+
 
 def main():
     conn = None
@@ -25,30 +42,29 @@ def main():
     try:
         while True:
             # print(it)
-            it +=1
+            it += 1
             file_line = input()
             json_line = json.loads(file_line)
             keys = list(json_line.keys())
-            if len(keys) != 1 :
+            if len(keys) != 1:
                 print("Unable to parse")
                 continue
             function_name = keys[0]
             args = json_line[function_name]
             fun_to_run = aviable_fucntions.get(function_name)
-            if fun_to_run == None :
-                print("No name of {} fucntion",function_name  )
+            if fun_to_run is None:
+                print("No name of {} fucntion", function_name)
                 continue
-            elif function_name == "open" :
-                conn,output = f.open_db(args)
+            elif function_name == "open":
+                conn, output = f.open_db(args)
             else:
                 output = aviable_fucntions[function_name](conn, args)
-            print( json.dumps(output
-                            #   ,indent=1, sort_keys=True
-                              ) )
-    except EOFError :
+            print(json.dumps(output, indent=1, sort_keys=True))
+    except EOFError:
         pass
     # conn.close()
     return 0
 
-if __name__ ==  "__main__" :
+
+if __name__ == "__main__":
     main()
